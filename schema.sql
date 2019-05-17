b9998420c3930730904ee7e60f1bb18eab697d07
anthon78 <economicsstudent2000@gmail.com>
2019-05-17 16:05:19 -0500

    refactor schema

--- schema.sql
+++ schema.sql
@@ -1,13 +1,33 @@
-DROP DATABASE IF EXISTS foshova;
+DROP DATABASE IF EXISTS fashova;
 
-CREATE DATABASE foshova;
+CREATE DATABASE fashova;
 
-USE foshova;
+USE fashova;
 
 CREATE TABLE womens_jeans(
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  uuid int NOT NULL,
-  product_name varchar(100),
-  image_url varchar(255),
+  uuid int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  url VARCHAR(255),
+  price int,
+  details__001 VARCHAR(255),
+  details__002 VARCHAR(255),
+  details__003 VARCHAR(255),
+  details__004 VARCHAR(255),
+  details__005 VARCHAR(255),
+  details__006 VARCHAR(255),
+  details__007 VARCHAR(255),
+  details__008 VARCHAR(255),
+  details__009 VARCHAR(255),
+  details__0010 VARCHAR(255),
+  images__001 VARCHAR(255),
+  images__002 VARCHAR(255),
+  images__003 VARCHAR(255),
+  images__004 VARCHAR(255),
+  images__005 VARCHAR(255),
+  images__006 VARCHAR(255),
+  images__007 VARCHAR(255),
+  images__008 VARCHAR(255),
+  item_category VARCHAR(255),
+  size_convention VARCHAR(255)
 )
 
