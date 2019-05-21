34e972bbbf726624131a14694770e75007e7aa8f
anthon78 <economicsstudent2000@gmail.com>
2019-05-21 17:07:29 -0500

    refactor h1 to typography component

--- client/components/info_bar.js
+++ client/components/info_bar.js
@@ -1,10 +1,16 @@
 import React from "react";
 import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import TypoGraphy from '@material-ui/core/Typography'
 const InfoBar = (props) => {
   return (
     <div>
-      <AppBar color="primary" position="static">
-        <h1>My header</h1>
+      <AppBar color="white" position="static">
+        <Toolbar>
+          <TypoGraphy variant="h1">
+            Fashova
+          </TypoGraphy>
+        </Toolbar>
       </AppBar>
     </div>
   )
