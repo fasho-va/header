6e9b0a4562ef8a3d3028b7804eb25e66514d4f41
anthon78 <economicsstudent2000@gmail.com>
2019-05-22 13:08:51 -0500

    add search glass icon

--- client/components/info_bar.js
+++ client/components/info_bar.js
@@ -5,6 +5,7 @@ import TypoGraphy from '@material-ui/core/Typography';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Autocomplete from 'react-autocomplete';
+import SearchIcon from '@material-ui/icons/Search';
 
 const InfoBar = (props) => {
   return (
@@ -34,6 +35,7 @@ const InfoBar = (props) => {
             onChange={(e) => value = e.target.value}
             onSelect={(val) => value = val}
           />
+          <SearchIcon />
         </Toolbar>
       </AppBar>
     </div>
