5f794ee98b2cd4600ae369efa58ec734edf03b61
anthon78 <economicsstudent2000@gmail.com>
2019-05-21 22:39:52 -0500

    add search bar

--- client/components/info_bar.js
+++ client/components/info_bar.js
@@ -1,15 +1,39 @@
 import React from "react";
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar'
-import TypoGraphy from '@material-ui/core/Typography'
+import Toolbar from '@material-ui/core/Toolbar';
+import TypoGraphy from '@material-ui/core/Typography';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Autocomplete from 'react-autocomplete';
+
 const InfoBar = (props) => {
   return (
     <div>
       <AppBar color="white" position="static">
         <Toolbar>
-          <TypoGraphy variant="h1">
+          <TypoGraphy variant="title">
             Fashova
           </TypoGraphy>
+          <Tabs>
+            <Tab label = "Women"/>
+            <Tab label = "Curve"/>
+            <Tab label = "Men"/>
+          </Tabs>
+          <Autocomplete
+            getItemValue={(item) => item.label}
+            items={[
+              { label: 'apple' },
+              { label: 'banana' },
+              { label: 'pear' }
+            ]}
+            renderItem={(item, isHighlighted) =>
+              <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+                {item.label}
+              </div>
+            }
+            onChange={(e) => value = e.target.value}
+            onSelect={(val) => value = val}
+          />
         </Toolbar>
       </AppBar>
     </div>
