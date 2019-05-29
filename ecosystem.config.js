30a218e8bac41f4d33501c0867ba1d8efde2f131
anthon78 <economicsstudent2000@gmail.com>
2019-05-29 16:50:37 -0500

    update config host

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-54-174-153-122.compute-1.amazonaws.com',
+      host: 'ec2-3-86-106-16.compute-1.amazonaws.com',
       key: '~/.ssh/fashova-header.pem',
       ref: 'origin/master',
       repo: 'https://github.com/anthon78/header',
