5356a0826bab559f212f7b38d52aa6048f8cc732
anthon78 <economicsstudent2000@gmail.com>
2019-05-29 11:49:22 -0500

    create retrieveAllNames

--- database/index.js
+++ database/index.js
@@ -5,9 +5,19 @@ const db = mysql.createConnection({
   user: "root",
   password : "",
   database: "fashova"
-})
+});
 
+const retrieveAllNames = (callback) => {
+  let sql = "SELECT name FROM womens_jeans";
+  db.query(sql, (err,names) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null,names);
+    }
+  })
+}
 
 module.exports = {
-  
+  retrieveAllNames,
 }
