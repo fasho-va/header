71340fd82104b886f526f0a60ece1f87cc9bb3d6
anthon78 <economicsstudent2000@gmail.com>
2019-05-29 12:07:24 -0500

    set up componentWillMount to retrieve names

--- client/app.js
+++ client/app.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "./components/header.js";
-
+import axios from "axios";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
+  }
+  componentWillMount() {
+    axios.get("http://localhost:3004/api/names/")
+    .then(names => {
+      console.log(names.data);
+    })
   }
 
   render() {
