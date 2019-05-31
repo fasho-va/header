eb7a76589a14c76077e5bbd38fe2d8ee965baf42
anthon78 <economicsstudent2000@gmail.com>
2019-05-31 12:19:09 -0500

    update font and icon styling

--- client/components/info_bar.js
+++ client/components/info_bar.js
@@ -20,24 +20,36 @@ const InfoBar = (props) => {
     e1.textDecoration = "none";
   }
 
+  const iconMouseOver = event => {
+    const e1 = event.target.style;
+    e1.transform = "scale(1.15)";
+  }
+
+  const iconMouseLeave = event => {
+    const e1 = event.target.style;
+    e1.transform = "scale(1.0)";
+  }
+
+
+
   return (
     <div>
       <AppBar color="white" position="static">
         <Toolbar>
-          <img src ={require("../logo.png")} style ={{height : "50px", width : "250px", marginLeft: "50px"}}/>  
-          <Tabs>
+          <img src ={require("../logo.png")} style ={{height : "50px", width : "250px", marginLeft: "50px", cursor: "pointer"}}/>  
+          <Tabs style = {{fontFamily : "Roboto"}}>
             <div style={{width : '70px', height: "60px", marginLeft : "15px", backgroundColor: "#000", color: "white", borderBottom :"5px solid black"}}>
-              <p onMouseOver = {hoverHandler} onMouseLeave = {mouseLeaveHandler} style={{paddingTop: "5px", paddingLeft: "10px"}}>
+              <p onMouseOver = {hoverHandler} onMouseLeave = {mouseLeaveHandler} style={{paddingTop: "5px", paddingLeft: "10px", cursor: "pointer"}}>
                 Women
               </p>
             </div>
             <div style={{width : '70px', backgroundColor: "white", color: "#000", border: "1px solid grey"}}>
-              <p onMouseOver = {hoverHandler} onMouseLeave = {mouseLeaveHandler} style={{paddingTop: "5px", paddingLeft: "15px"}}>
+              <p onMouseOver = {hoverHandler} onMouseLeave = {mouseLeaveHandler} style={{paddingTop: "5px", paddingLeft: "15px", cursor: "pointer"}}>
                 Curve
               </p>
             </div>
             <div style={{width : '70px', backgroundColor: "white", color: "#000", border: "1px solid grey"}}>
-              <p onMouseOver = {hoverHandler} onMouseLeave = {mouseLeaveHandler} style={{paddingTop: "5px", paddingLeft: "20px"}}>
+              <p onMouseOver = {hoverHandler} onMouseLeave = {mouseLeaveHandler} style={{paddingTop: "5px", paddingLeft: "20px", cursor: "pointer"}}>
                 Men
               </p>
             </div>
@@ -45,16 +57,17 @@ const InfoBar = (props) => {
           <div style={{marginLeft : "20px"}}>
             <Search productInfo={props.productInfo} updateUuid = {props.updateUuid}/>
           </div>
-          <div style={{width: "100px", backgroundColor: "black", marginRight: "20px"}}>
+          <div style={{width: "100px", backgroundColor: "black", marginRight: "20px", cursor: "pointer"}}>
             <SearchIcon style ={{color: "white", marginLeft: "35px"}}/>
           </div>
           <div>
-            <PersonIcon />
+            <PersonIcon style ={{cursor: "pointer"}} onMouseOver = {iconMouseOver} onMouseLeave = {iconMouseLeave} />
           </div>
           <div style={{margin: "10px"}}>
-            <FavoriteIcon /></div>
+            <FavoriteIcon  style ={{cursor: "pointer"}} onMouseOver = {iconMouseOver} onMouseLeave = {iconMouseLeave}/>
+          </div>
           <div style={{marginLeft:"5px"}}>
-            <CardTravelIcon />
+            <CardTravelIcon  style ={{cursor: "pointer"}} onMouseOver = {iconMouseOver} onMouseLeave = {iconMouseLeave}/>
           </div>
         </Toolbar>
       </AppBar>
